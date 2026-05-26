ecc2f69d977aa456d46b8403a6cc929ee5c00d6a
Juan Ardila <ardilajuan0304@gmail.com>
2026-05-26 16:18:21 -0500

    Add pet information and vaccination status logging

---
+++ reto2Mascota.js
@@ -0,0 +1,8 @@
+const nombre="Toby";
+const especie="Bull Terrier";
+const edad=4;
+const vacunada=true;
+const dueño="Juan A";
+console.log("-------------------------------------------------------------------------------------------------");
+const mensaje=`Mi mascota se llama ${nombre} es un perro de ${edad} años, y es  un ${especie}, actualmente su estado de vacunacion es: ${vacunada} y El dueño es: ${dueño}`;
+console.log(mensaje);
