c1dbb8a0c42374bad52e0c17625b0f1eaf25ac37
Juan Ardila <ardilajuan0304@gmail.com>
2026-05-27 21:45:56 -0500

    Validacion Acceso-reto js

---
+++ validacionAcceso.js
@@ -0,0 +1,21 @@
+const nombre1="Juan";
+const edad=17;
+const administrador=true;
+const cuentaActiva=true;
+let puedeEntrar=0;
+switch(nombre1){
+    case "Juan":
+        if(edad>=18 || administrador===true){
+            console.log("Es mayor de edad o administrador por ende, puede Ingresar :)")
+            puedeEntrar++;
+        }else if(edad>=18 && cuentaActiva===false){
+            console.log("Es mayor de edad pero su cuenta es inactiva")
+
+        }else if(edad<18 && cuentaActiva===false){
+            console.log("No puede ingresar porque es menor de edad");
+        }else if(edad>=18 && cuentaActiva===true){
+            console.log("Puede entrar, bienvenido");
+            puedeEntrar++;
+        }
+        console.log("La cantidad de usuarios que ingresaron son:"+puedeEntrar);
+}
