04977939bf871c1869eec187f798dadcaa7c8a88
Juan Ardila <ardilajuan0304@gmail.com>
2026-05-27 22:32:42 -0500

    EJERCICIOS VARIABLES (RETO 2)

--- reto2Mascota.js
+++ reto2Mascota.js
@@ -1,3 +1,8 @@
+/*Crea variables para una mascota: nombre, especie, edad, vacunada (boolean) y 
+dueño (puede ser null si no tiene). Imprime un mensaje usando template literal que 
+diga: 'Mi mascota Firulais es un Perro de 3 años. Vacunada: sí. Dueño: aún no 
+registrado.' adaptándose a los valores que pongas.
+*/
 const nombre="Toby";
 const especie="Bull Terrier";
 const edad=4;
