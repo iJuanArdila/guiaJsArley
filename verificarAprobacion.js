483674f80575cedd426216b5d68639dc12e1657f
Juan Ardila <ardilajuan0304@gmail.com>
2026-05-27 22:35:14 -0500

    EJERCICIO TERNARIO (RETO 1)
    Added comments explaining the purpose of the code and its functionality.

--- verificarAprobacion.js
+++ verificarAprobacion.js
@@ -1,3 +1,8 @@
+/*Dado el promedio de un estudiante, usa un operador ternario para asignar a una 
+variable resultado el valor 'APROBADO' si el promedio es 3.0 o más, o 
+'REPROBADO' en otro caso. Imprime el nombre del estudiante y su resultado en 
+un template literal.
+*/
 const nombre="Juan A";
 let promedio=4 
 const aprobadoYreprobado=promedio >=3 ? "APROBADO": "REPROBADO";
