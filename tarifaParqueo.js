fc8d0ed89fe6039e1a4ba941b1502b993a0d9fa0
Juan Ardila <ardilajuan0304@gmail.com>
2026-05-27 22:35:44 -0500

    EJERCICIO TERNARIO (RETO 2)

--- tarifaParqueo.js
+++ tarifaParqueo.js
@@ -1,3 +1,8 @@
+/*Crea variables para tipo (carro, moto o bicicleta) y horas. Usa un ternario ANIDADO 
+para asignar el valor por hora: 3000 si es carro, 1500 si es moto, 0 si es bicicleta. 
+Calcula y muestra el total a pagar formateado.
+*/
+
 const nombre="Juan"
 let tipoVehiculo="Carro"
 let horas=28;
